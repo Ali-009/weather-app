2408abc9033132757c30a9994af2c83ca8f8a862
AliTinai <ali-009@windowslive.com>
2022-04-10 13:25:29 +0400

    Weather Data can now be displayed on the page

--- index.js
+++ index.js
@@ -1,25 +1,57 @@
 
-/*This function will take in the location as a string and return weather data*/
+let weatherPromise;
+let dataElement = document.querySelector('#temp-data');
 
-function queryOpenWeather(cityInput){
-
-  let weatherData;
+async function getWeatherData(locationInput){
 
   let apiKey = '9ccfde44cd99c120bad6a7b986a92fb2';
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}`)
-    .then(function(response){
-      return response.json();
-    })
-    .then(function(data){
-      weatherKelvin = data.main.temp;
-      weatherCelsius = weatherKelvin - 273.15;
-      console.log(weatherCelsius);
-    }).catch(console.log);
+
+  let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${locationInput}&appid=${apiKey}`);
+
+  if(!response.ok){
+    throw 'Weather Data Not Found';
+  }
+
+  let data = await response.json();
+
+  let weatherKelvin = data.main.temp;
+  let weatherCelsius = Math.round(weatherKelvin - 273.15);
+  let weatherFahrenheit = Math.round(weatherKelvin * (9/5) - 459.67);
+
+  return {weatherCelsius, weatherFahrenheit};
 }
 
-/*async function queryOpenWeather(cityInput){
-  let weatherData;
-}*/
+let formElement = document.querySelector('#location-form');
+formElement.addEventListener('submit', (e) => {
+  //prevent the default behavior of refreshing the page
+  e.preventDefault();
 
-let city = prompt('Enter Location');
-queryOpenWeather(city);
+  let locationInputElement = document.querySelector('#location-input');
+
+  weatherPromise = getWeatherData(locationInputElement.value);
+
+  weatherPromise.then((data) => {
+    dataElement.textContent = data.weatherCelsius + ' C°';
+  }).catch((err) => {
+    dataElement.textContent = '';
+
+    let errorMessage = document.querySelector('#error-message');
+    errorMessage.textContent = err;
+  });
+});
+
+let UnitToggleButton = document.querySelector('#unit-toggle');
+UnitToggleButton.addEventListener('click', () => {
+
+  weatherPromise.then((data) => {
+    if(dataElement.value === 'c'){
+      dataElement.value = 'f';
+      dataElement.textContent = data.weatherFahrenheit + ' F°';
+    } else {
+      dataElement.value = 'c';
+      dataElement.textContent = data.weatherCelsius + ' C°';
+    }
+  }).catch((err) => {
+    
+  });
+});
